1be606f5c76b358a4faa196314ab1a3648c442df
Tom Ritland <t_ritland@yahoo.com>
2017-08-04 17:47:02 -0500

    clicks create object with number inputs and operator type

--- public/client.js
+++ public/client.js
@@ -1,18 +1,29 @@
-$(document).ready(function() {
-    $('#addButton').on('click', function(){
-        console.log('add clicked');
-    });
-    
-    $('#subtractButton').on('click', function(){
-        console.log('subtract clicked');
+$(document).ready(function () {
+    $('#addButton').on('click', function () {
+        console.log(objectBuilder('Add'));
     });
 
-    $('#multiplyButton').on('click', function(){
-        console.log('multiply clicked');
+    $('#subtractButton').on('click', function () {
+        console.log(objectBuilder('Subtract'));
     });
 
-    $('#divideButton').on('click', function(){
-        console.log('divide clicked');
+    $('#multiplyButton').on('click', function () {
+        console.log(objectBuilder('Multiply'));
     });
 
+    $('#divideButton').on('click', function () {
+        console.log(objectBuilder('Divide'));
+    });
 });
+
+function objectBuilder(operator) {
+    var calcExpression = {
+        x: parseInt($('#firstNumber').val()),
+        y: parseInt($('#secondNumber').val()),
+        type: operator
+    }
+    return calcExpression;
+    //will eventually send calcExpression to AJAX POST 
+}
+
+//AJAX POST goes here
