f7dad85b9b5dec2bb0350b969c19b9444b1aa3a2
Tom Ritland <t_ritland@yahoo.com>
2017-08-04 16:52:18 -0500

    buttons click

--- public/client.js
+++ public/client.js
@@ -1,5 +1,18 @@
-console.log('js is sourced');
-
 $(document).ready(function() {
-    console.log('jquery is sourced');
+    $('#addButton').on('click', function(){
+        console.log('add clicked');
+    });
+    
+    $('#subtractButton').on('click', function(){
+        console.log('subtract clicked');
+    });
+
+    $('#multiplyButton').on('click', function(){
+        console.log('multiply clicked');
+    });
+
+    $('#divideButton').on('click', function(){
+        console.log('divide clicked');
+    });
+
 });
